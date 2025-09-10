6c5488c1af73dbb6d8115fe1361826452cbec9bd
piecebypiece <chlx357@gmail.com>
2025-09-10 17:35:04 +0900

    Update GameRuleData 에디터 노출 시키는 작업

--- Source/TFD/GameData/FGameRuleData.h
+++ Source/TFD/GameData/FGameRuleData.h
@@ -10,11 +10,16 @@ struct FGameRuleData
 {
 	GENERATED_BODY()
 
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
 	float PlayTimeSec = 90.f;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
 	float PoliceRatio = 0.5f;		//경찰 비율 올림으로 계산 최소 1명 최대 2명
 
-	TArray<TSoftObjectPtr<AActor*>> PlayerTransformThief;
-	TArray<TSoftObjectPtr<AActor*>> PlayerTransformPolice;
+	UPROPERTY(EditAnywhere)
+	TArray<TSoftObjectPtr<AActor>> PlayerTransformThief;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	TArray<TSoftObjectPtr<AActor>> PlayerTransformPolice;
 
 	//TSubclassOf
 	// 캐릭터 액터 에셋
