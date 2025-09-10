9f4f6191bb08dd89307a002b2282e71ff4be2adb
LEEJS256 <68937552+LEEJS256@users.noreply.github.com>
2025-09-10 21:13:30 +0900

    fix(GameMode) : 게임모드에 스폰기능 추가,플레이어 랜덤위치 이동

--- Source/TFD/GameMode/TFDGameMode.h
+++ Source/TFD/GameMode/TFDGameMode.h
@@ -6,6 +6,8 @@
 #include "GameFramework/GameMode.h"
 #include "TFDGameMode.generated.h"
 
+class ATFDCharacter;
+class ATFDSpawnVolume;
 /**
  * 
  */
@@ -13,5 +15,31 @@ UCLASS()
 class TFD_API ATFDGameMode : public AGameMode
 {
 	GENERATED_BODY()
+protected:
+	virtual void BeginPlay() override;
+	virtual void PostLogin(APlayerController* NewPlayer) override;
+	//스폰위치 결정짓는 함수
+	virtual AActor* ChoosePlayerStart_Implementation(AController* Player) override;
+
+  
+protected:
+	UPROPERTY()
+	TArray<ATFDSpawnVolume*> SpawnVolumes;
+
+	// 헤더 파일(.h)에 추가
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Spawn")
+	TSubclassOf<ATFDCharacter> CorpCharacterClass;
+	
+public:
+	void SpawnAI(int32 NumberOfPeople = 1);
+	void SpawnPlayer(int32 NumberOfPeople = 1);
+private:
+	FVector GetRandomPointInSpawnArea();
+	void	InitializeSpawnVolumes();
+	void	MovePlayerToRandomSpawnPoint(APlayerController* PlayerController);
+private:
+	FTimerHandle PlayerSpawnTimerHandle;
 	
 };
+
+
