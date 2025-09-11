f9d8895a10d9b3d2702be3423d06475c0b877102
piecebypiece <chlx357@gmail.com>
2025-09-11 17:27:17 +0900

    Update GameState 의 종류별 플레이어 정보 리스트를 공개 변수로 변경

--- Source/TFD/GameState/TFDGameState.h
+++ Source/TFD/GameState/TFDGameState.h
@@ -24,6 +24,13 @@ public:
 
 	const int MinimumPlayerNum = 2;
 
+	// HACK: 캡슐화 하기 번거로워서 공개 변수로 둠.
+	// 도둑정보 Controller 리스트
+	TArray<TSoftObjectPtr< ATFDPlayerState>> PolicePlayerStateArray;
+	// 경찰정보 Controller 리스트
+	TArray<TSoftObjectPtr< ATFDPlayerState>> ThiefPlayerStateArray;
+
+	TArray<TSoftObjectPtr<ATFDPlayerState>> CaughtThiefPlayerStateArray;
 protected:
 	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (AllowPrivateAccess));
 	FGameRuleData GameRuleData;		//TODO: DA 로 변경
@@ -33,16 +40,10 @@ protected:
 	UPROPERTY(ReplicatedUsing = OnRep_GameStateChange, BlueprintReadOnly)
 	EGameState GameState;	// 게임상태 변경처리 OnRep 함수 생성하여 후처리
 	
-	// 현재 게임 상태필요
-	// 도둑정보 Controller 리스트
-	TArray<TSoftObjectPtr< ATFDPlayerState>> PolicePlayerStateArray;
-	// 경찰정보 Controller 리스트
-	TArray<TSoftObjectPtr< ATFDPlayerState>> ThiefPlayerStateArray;
-
 	UPROPERTY(Replicated)
 	TArray<ATFDPlayerState*> ReadyPlayers;
 
-	// 게임 시작 알림용 Multicast
+	// 게임 상태 변환을 알림
 	UFUNCTION()
 	void OnRep_GameStateChange();
 };
