e9f87b37236a45eef95eff00c60cdc5ea871f775
LEEJS256 <68937552+LEEJS256@users.noreply.github.com>
2025-09-16 14:51:35 +0900

    feat(DataTatble) : DT AllowTeamTag 선언

---
+++ Source/TFD/Object/AllowTeamTag.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataTable.h"
+#include "GameplayTagContainer.h"
+#include "AllowTeamTag.generated.h"
+
+//실수로 TFD안붙힘
+USTRUCT(BlueprintType)
+struct FAllowTeamTag : public FTableRowBase
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Tag")
+	FGameplayTag ItemTag;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Tag")
+	TArray<FGameplayTag>		AllowedTeamTag;
+
+	// 스폰할 아이템
+	UPROPERTY(EditAnywhere, BlueprintReadOnly , Category="Tag")
+	TSubclassOf<AActor> ItemClass;
+};
