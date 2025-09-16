f1313bdccc6e79954bd410ecad90f546bb6f3fd5
LEEJS256 <68937552+LEEJS256@users.noreply.github.com>
2025-09-16 14:52:02 +0900

    feat(Spawnpoint) : Spawnpoint액터추가

---
+++ Source/TFD/Object/TFDSpawnpoint.h
@@ -0,0 +1,48 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "GameFramework/Actor.h"
+#include "TFDSpawnpoint.generated.h"
+
+UCLASS()
+class TFD_API ATFDSpawnpoint : public AActor
+{
+	GENERATED_BODY()
+	
+public:
+	// Sets default values for this actor's properties
+	ATFDSpawnpoint();
+
+public:
+	void SpawnItem();
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	FVector GetSpawnPointLocation();
+	TArray<FGameplayTag> GetAllowedTeamTag();
+
+	bool CheckItemTag(FGameplayTag InTag);
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+protected:
+	UPROPERTY(VisibleAnywhere, Category="Visual")
+	class UBillboardComponent* Billboard;
+
+	UPROPERTY(EditAnywhere, Category="GAS")
+	FGameplayTag		ItemTag;
+
+	UPROPERTY(VisibleAnywhere,BlueprintReadOnly, Category="GAS")
+	TArray<FGameplayTag>		AllowedTeamTag;
+
+	// 스폰할 아이템
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly , Category="Tag")
+	TSubclassOf<AActor> ItemClass;
+
+private:
+	void SetAllowedTeamTag();
+};
