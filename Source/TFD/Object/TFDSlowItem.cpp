680c2073e228bffc8a9672aaa2b0432b49f4e699
LEEJS256 <68937552+LEEJS256@users.noreply.github.com>
2025-09-18 10:56:36 +0900

    feat(item) : 기존 골드바/슬로우 아이템 대체

---
+++ Source/TFD/Object/TFDSlowItem.cpp
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Object/TFDSlowItem.h"
+
+ATFDSlowItem::ATFDSlowItem()
+{
+}
+
+void ATFDSlowItem::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void ATFDSlowItem::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
