ac79ba94ec7754aeea5383cc41dc79d8bc2eef12
piecebypiece <chlx357@gmail.com>
2025-09-19 21:24:42 +0900

    Add GameplayCue 를 위한 작업 중.

---
+++ Source/TFD/GameAbilitySystem/GameplayCue/GCN_PlayMontage.cpp
@@ -0,0 +1,10 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GCN_PlayMontage.h"
+
+void AGCN_PlayMontage::HandleGameplayCue(AActor* MyTarget, EGameplayCueEvent::Type EventType,
+	const FGameplayCueParameters& Parameters)
+{
+	Super::HandleGameplayCue(MyTarget, EventType, Parameters);
+}
