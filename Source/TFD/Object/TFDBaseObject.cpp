72b4b181beefeb5bee6bad5bbe5276dd2f45d20d
LEEJS256 <68937552+LEEJS256@users.noreply.github.com>
2025-09-22 19:31:36 +0900

    fix(BaseObject) : base오브젝트 투사체 기능추가

--- Source/TFD/Object/TFDBaseObject.cpp
+++ Source/TFD/Object/TFDBaseObject.cpp
@@ -2,11 +2,16 @@
 
 
 #include "Object/TFDBaseObject.h"
+#include "NiagaraFunctionLibrary.h"
+#include "AbilitySystemBlueprintLibrary.h"
 #include "Components/SphereComponent.h"
 #include "Components/StaticMeshComponent.h"
 #include "AbilitySystemComponent.h"
+#include "TFDNativeGameplayTags.h"
 #include "Character/TFDCharacterBase.h"
+#include "Component/TFDProjectileMovementComponent.h"
 #include "GameMode/TFDGameMode.h"
+#include "Kismet/GameplayStatics.h"
 
 // Sets default values
 ATFDBaseObject::ATFDBaseObject()
@@ -22,6 +27,53 @@ void ATFDBaseObject::BeginPlay()
 
 	CollisionComp->OnComponentBeginOverlap.AddDynamic(this, &ATFDBaseObject::OnOverlapBegin);
 	SetAllowedTeamTag();
+
+
+	if (GetOwner())
+	{
+		UAbilitySystemComponent* OwnerAsc = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(GetOwner());
+		if (OwnerAsc)
+		{
+			// 주인의 태그 중 Team 카테고리에 속하는 태그를 찾아서 저장.
+			OwnerTeamTag = OwnerAsc->GetOwnedGameplayTags().Filter(FGameplayTagContainer(TAG_Team)).
+									 First();
+		}
+	}
+
+	StartTransform = GetActorTransform();
+	switch (BaseObjectParam.MovementType)
+	{
+	case EProjectileMovementType::Arc:
+
+		break;
+
+	case EProjectileMovementType::Straight:
+	default:
+		ProjectileMovementComponent->Velocity = GetActorForwardVector() * ProjectileMovementComponent->InitialSpeed;
+		break;
+	}
+
+
+	if (BaseObjectParam.MuzzleVFX)
+	{
+		UNiagaraComponent* MuzzleVFXComponent = UNiagaraFunctionLibrary::SpawnSystemAtLocation(
+			this,
+			BaseObjectParam.MuzzleVFX,
+			StartTransform.GetLocation(),
+			StartTransform.GetRotation().Rotator(),
+			BaseObjectParam.MuzzleScale,
+			true
+		);
+		
+	}
+
+	if (BaseObjectParam.MuzzleSound)
+	{
+		UGameplayStatics::PlaySoundAtLocation(this, BaseObjectParam.MuzzleSound, StartTransform.GetLocation());
+	}
+	
+	
+	
 }
 
 void ATFDBaseObject::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
@@ -73,6 +125,8 @@ void ATFDBaseObject::Tick(float DeltaTime)
 void ATFDBaseObject::DefaultCreater()
 {
 	bReplicates = true;
+	SetReplicateMovement(true);
+
 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = false;
 
@@ -90,6 +144,26 @@ void ATFDBaseObject::DefaultCreater()
 	MeshComp->SetupAttachment(RootComponent);
 	//메시는 충돌안함
 	MeshComp->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+
+	ProjectileMovementComponent = CreateDefaultSubobject<UTFDProjectileMovementComponent>(
+		TEXT("TFDProjectileMovementComponent"));
+	//발사체 움직임 컴포넌트
+	if (BaseObjectParam.bMoveFlag)
+	{
+		ProjectileMovementComponent->bAutoActivate = true;
+		ProjectileMovementComponent->InitialSpeed = BaseObjectParam.ProjectileSpeed;
+		ProjectileMovementComponent->MaxSpeed = BaseObjectParam.ProjectileSpeed;
+		//중력 영향X
+		ProjectileMovementComponent->ProjectileGravityScale = 0.f;
+		// 투사체가 속도방향을 따라 회전
+		ProjectileMovementComponent->bRotationFollowsVelocity = true;
+	}
+	else
+	{
+		ProjectileMovementComponent->bAutoActivate = false;
+		ProjectileMovementComponent->InitialSpeed = 0.f;
+		ProjectileMovementComponent->MaxSpeed = 0.f;
+	}
 }
 
 void ATFDBaseObject::SetAllowedTeamTag()
--- Source/TFD/Object/TFDBaseObject.h
+++ Source/TFD/Object/TFDBaseObject.h
@@ -3,14 +3,39 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "GameplayEffectTypes.h"
 #include "GameplayTagContainer.h"
 #include "GameFramework/Actor.h"
+#include "NiagaraSystem.h"
 #include "TFDBaseObject.generated.h"
 
-
+class UTFDProjectileMovementComponent;
 class USphereComponent;
 class UStaticMeshComponent;
 class UGameplayEffect;
+
+
+UENUM(BlueprintType)
+enum class EImpactRotationMethod : uint8
+{
+	// 충돌 지점의 표면 각도에 맞춰 회전합니다.
+	AlignToImpactNormal,
+	// 투사체가 날아가던 방향을 유지합니다.
+	AlignToProjectile,
+	// 항상 기본 회전값(0,0,0)을 사용합니다.
+	ZeroRotation
+};
+
+
+UENUM(BlueprintType)
+enum class EProjectileMovementType : uint8
+{
+	// 직선 이동: 일정한 방향과 속도로 곧게 이동
+	Straight UMETA(DisplayName = "Straight"),
+	// 포물선 이동: 중력 영향을 받아 곡선을 그리며 이동
+	Arc UMETA(DisplayName = "Arc")
+};
+
 
 USTRUCT(BlueprintType)
 struct FTFDBaseObjectParam
@@ -18,6 +43,72 @@ struct FTFDBaseObjectParam
 	GENERATED_BODY()
 
 	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "TFD|Projectile|Move",
+		meta = (DisplayName = "Moveflag", EditConditionHides))
+	bool bMoveFlag = false;
+
+	// 투사체의 이동 방식을 결정
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "TFD|Projectile|Movement", meta = (DisplayName = "이동 방식"))
+	EProjectileMovementType MovementType = EProjectileMovementType::Straight;
+	
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "TFD|Projectile|Move",
+		meta = (DisplayName = "가속도", EditConditionHides))
+	float ProjectileSpeed = 1500.f;
+	
+	// true이면 비행 중 주변의 적을 탐지하고 유도
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "TFD|Projectile|Homing", meta = (DisplayName = "유도 기능 활성화"))
+	bool bEnableHoming = false;
+	
+	// 발사 시 재생할 사운드
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "TFD|Projectile|Effects|Muzzle",
+		meta = (DisplayName = "발사 사운드"))
+	TObjectPtr<USoundBase> MuzzleSound;
+
+	// 발사 시 스폰할 나이아가라 이펙트
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "TFD|Projectile|Effects|Muzzle",
+		meta = (DisplayName = "발사 이펙트(VFX)"))
+	TObjectPtr<UNiagaraSystem> MuzzleVFX;
+
+	// 발사 이펙트의 크기를 조절
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "TFD|Projectile|Effects|Muzzle",
+		meta = (DisplayName = "발사 이펙트 스케일", EditCondition = "MuzzleVFX != nullptr"))
+	FVector MuzzleScale = FVector::OneVector;
+
+	// 발사 이펙트의 재생 속도를 조절 (1.0 = 100%)
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "TFD|Projectile|Effects|Muzzle",
+		meta = (DisplayName = "발사 이펙트 속도", EditCondition = "MuzzleVFX != nullptr", ClampMin = "0.01", UIMin = "0.01"))
+	float MuzzleVFXSpeed = 1.0f;
+
+	// 충돌 시 재생할 사운드
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "TFD|Projectile|Effects|Impact",
+		meta = (DisplayName = "충돌 사운드"))
+	TObjectPtr<USoundBase> ImpactSound;
+
+	// 충돌 시 스폰할 나이아가라 이펙트
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "TFD|Projectile|Effects|Impact",
+		meta = (DisplayName = "충돌 이펙트(VFX)"))
+	TObjectPtr<UNiagaraSystem> ImpactVFX;
+
+	// 충돌 이펙트의 크기를 조절
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "TFD|Projectile|Effects|Impact",
+		meta = (DisplayName = "충돌 이펙트 스케일", EditCondition = "ImpactVFX != nullptr"))
+	FVector ImpactScale = FVector::OneVector;
+
+	// 충돌 이펙트의 재생 속도를 조절 (1.0 = 100%)
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "TFD|Projectile|Effects|Impact",
+		meta = (DisplayName = "충돌 이펙트 속도", EditCondition = "ImpactVFX != nullptr", ClampMin = "0.01", UIMin = "0.01"))
+	float ImpactVFXSpeed = 1.0f;
+
+	// 충돌 이펙트의 초기 회전 방식을 결정
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "TFD|Projectile|Effects|Impact",
+		meta = (DisplayName = "충돌 이펙트 회전 방식", EditCondition = "ImpactVFX != nullptr"))
+	EImpactRotationMethod RotationMethod = EImpactRotationMethod::AlignToImpactNormal;
+
+	// 충돌 지점으로부터 이펙트가 얼마나 떨어져서 스폰될지 결정 (cm 단위)
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "TFD|Projectile|Effects|Impact",
+		meta = (DisplayName = "충돌 이펙트 오프셋", EditCondition = "ImpactVFX != nullptr"))
+	float ImpactOffset = 0.0f;
 };
 UCLASS()
 class TFD_API ATFDBaseObject : public AActor
@@ -48,6 +139,9 @@ protected:
 	// 적용할 GameplayEffect (블루프린트에서 할당)
 	UPROPERTY(EditAnywhere, Category = "GAS")
 	TSubclassOf<UGameplayEffect> CollisionEffect;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "TFD|Components")
+	TObjectPtr<UTFDProjectileMovementComponent> ProjectileMovementComponent;
 
 	UPROPERTY(EditAnywhere, Category="GAS")
 	FGameplayTag		ItemTag;
@@ -55,6 +149,12 @@ protected:
 	UPROPERTY(VisibleAnywhere,BlueprintReadOnly, Category="GAS")
 	FGameplayTagContainer		AllowedTeamTag;
 
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (ShowOnlyInnerProperties))
+	FTFDBaseObjectParam BaseObjectParam;
+
+
+	FGameplayEffectContextHandle CachedContext;
+	TSubclassOf<UGameplayEffect> DamageEffectClass;
 	
 public:	
 	// Called every frame
@@ -62,6 +162,15 @@ public:
 
 	const FGameplayTagContainer& GetAllowedTeamTag() const { return AllowedTeamTag; }
 
+	void SetBaseObjectParam(const FTFDBaseObjectParam&  InBaseObjectParams) {BaseObjectParam = InBaseObjectParams; }
+	// void InitEffectContext(FGameplayEffectContextHandle InContext, TSubclassOf<UGameplayEffect> InDamageEffect);
+	// void OnHit(AActor* OtherActor);
+
+	
 private:
 	void SetAllowedTeamTag();
+
+private:
+	FGameplayTag OwnerTeamTag;
+	FTransform StartTransform = FTransform();
 };
