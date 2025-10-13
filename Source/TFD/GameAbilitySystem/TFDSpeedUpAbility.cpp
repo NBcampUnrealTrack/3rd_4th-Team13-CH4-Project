db02f5e218919cc30a0a4a3fb4e3058640cf2636
lambone23 <lambone234567@gmail.com>
2025-10-13 15:51:10 +0900

    feat(ability): SpeedUp 어빌리티 클래스 추가
    - 작업물: TFDSpeedUpAbility.h/cpp
    - 작업 내용:
       - SpeedUp 효과를 위한 TFDSpeedUpAbility C++ 클래스 추가
       - GAS 기반 속도 증가 로직 구현을 위한 기반 마련

---
+++ Source/TFD/GameAbilitySystem/TFDSpeedUpAbility.cpp
@@ -0,0 +1,57 @@
+﻿// TFDSpeedUpAbility.cppS
+#include "GameAbilitySystem/TFDSpeedUpAbility.h"
+#include "AbilitySystemComponent.h"
+
+UTFDSpeedUpAbility::UTFDSpeedUpAbility()
+{
+	InstancingPolicy = EGameplayAbilityInstancingPolicy::InstancedPerActor;
+}
+
+void UTFDSpeedUpAbility::ActivateAbility(
+	const FGameplayAbilitySpecHandle Handle,
+	const FGameplayAbilityActorInfo* ActorInfo,
+	const FGameplayAbilityActivationInfo ActivationInfo,
+	const FGameplayEventData* TriggerEventData)
+{
+	if (!CommitAbility(Handle, ActorInfo, ActivationInfo))
+	{
+		EndAbility(Handle, ActorInfo, ActivationInfo, true, true);
+		return;
+	}
+
+	AActor* AvatarActor = ActorInfo->AvatarActor.Get();
+
+	if (!AvatarActor)
+	{
+		EndAbility(Handle, ActorInfo, ActivationInfo, true, true);
+		return;
+	}
+
+	if (!Effect)
+	{
+		EndAbility(Handle, ActorInfo, ActivationInfo, true, true);
+		return;
+	}
+
+	FGameplayEffectContextHandle Context = ActorInfo->AbilitySystemComponent->MakeEffectContext();
+	FGameplayEffectSpecHandle SpecHandle = ActorInfo->AbilitySystemComponent->MakeOutgoingSpec(Effect, 1.f, Context);
+	if (!SpecHandle.IsValid())
+	{
+		EndAbility(Handle, ActorInfo, ActivationInfo, true, true);
+		return;
+	}
+	UE_LOG(LogTemp, Warning, TEXT("[UTFDSpeedUpAbility][ActivateAbility] Effect 실행"));
+	SpecHandle.Data->SetDuration(Duration, true);
+	ActorInfo->AbilitySystemComponent->ApplyGameplayEffectSpecToSelf(*SpecHandle.Data);
+
+	EndAbility(Handle, ActorInfo, ActivationInfo, true, false);
+}
+
+void UTFDSpeedUpAbility::EndAbility(
+	const FGameplayAbilitySpecHandle Handle,
+	const FGameplayAbilityActorInfo* ActorInfo,
+	const FGameplayAbilityActivationInfo ActivationInfo,
+	bool bReplicateEndAbility, bool bWasCancelled)
+{
+	Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
+}
---
+++ Source/TFD/GameAbilitySystem/TFDSpeedUpAbility.h
@@ -0,0 +1,35 @@
+﻿// TFDSpeedUpAbility.h
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameAbilitySystem/Ability/TFDGameplayAbility.h"
+#include "TFDSpeedUpAbility.generated.h"
+
+UCLASS()
+class TFD_API UTFDSpeedUpAbility : public UTFDGameplayAbility
+{
+	GENERATED_BODY()
+
+public:
+    UTFDSpeedUpAbility();
+
+    virtual void ActivateAbility(
+        const FGameplayAbilitySpecHandle Handle,
+        const FGameplayAbilityActorInfo* ActorInfo,
+        const FGameplayAbilityActivationInfo ActivationInfo,
+        const FGameplayEventData* TriggerEventData) override;
+
+    virtual void EndAbility(
+        const FGameplayAbilitySpecHandle Handle,
+        const FGameplayAbilityActorInfo* ActorInfo,
+        const FGameplayAbilityActivationInfo ActivationInfo,
+        bool bReplicateEndAbility,
+        bool bWasCancelled) override;
+
+protected:
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "TFD|Ability|Effects")
+    TSubclassOf<UGameplayEffect> Effect;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "TFD|Ability|Effects")
+    float Duration;
+};
